a5ccdbdb1394bc448da81916ad52e426793fa5ee
ATK <ATKmain@gmail.com>
2016-06-03 16:17:05 +1000

    Improve and simplyfy function string_to_selectable_table

--- PLSQL/string_to_selectable_table_function.sql
+++ PLSQL/string_to_selectable_table_function.sql
@@ -1,14 +1,11 @@
 -- A method to convert a delimited string like checkbox items' value in APEX to a selectable table to use simply in SQL queries
-
-CREATE or REPLACE TYPE t_row AS OBJECT (
-  id           NUMBER,
-  description  VARCHAR2(4000)
-);
-
-CREATE or REPLACE TYPE t_row_tab IS TABLE OF t_row ;
-
+Drop function string_to_selectable_table;
+Drop TYPE t_str_row_tab ;
+/
+CREATE or REPLACE TYPE t_str_row_tab IS TABLE OF VARCHAR2(4000) ;
+/
 CREATE or REPLACE FUNCTION string_to_selectable_table ( p_string      IN VARCHAR2,   p_delimiter   IN VARCHAR2 DEFAULT ':')
-      RETURN t_row_tab PIPELINED
+      RETURN t_str_row_tab PIPELINED
    AS
       n_row NUMBER := 1 ;
       v_str VARCHAR2(4000) ;
@@ -16,7 +13,7 @@ CREATE or REPLACE FUNCTION string_to_selectable_table ( p_string      IN VARCHAR
       v_str := trim(regexp_substr(p_string, '[^'|| p_delimiter || ']+', 1, n_row) ) ;
       WHILE ( v_str is not null )
       LOOP
-          PIPE ROW ( t_row( n_row ,  v_str) );
+          PIPE ROW ( v_str );
           n_row := n_row + 1 ;
           v_str := trim(regexp_substr(p_string, '[^'|| p_delimiter || ']+', 1, n_row) ) ;
           
@@ -24,7 +21,7 @@ CREATE or REPLACE FUNCTION string_to_selectable_table ( p_string      IN VARCHAR
 
       RETURN;
 END string_to_selectable_table;
-
+/
 -- Example Query
-select id,description  from table(string_to_selectable_table('Option1:Option2:Option3', ':') ) aa;
+select COLUMN_VALUE  from table(string_to_selectable_table('Option1:Option2:Option3', ':') ) aa;
    
